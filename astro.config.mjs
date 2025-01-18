c8771d5c5a417a591fc3d208f3ae5459ec9c3c08
jhonattan2121 <devferri22@gmail.com>
2025-01-18 15:05:34 -0300

    broken static and serves fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
   },
  
   output: 'static',
+  adapter: vercel(),
   content: {
     collections: ['docs'],
   },
